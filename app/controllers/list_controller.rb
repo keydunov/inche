f1c425f42556c9a95e95ef35787b847f564280dd
Artyom Keydunov <artyom.keydunov@gmail.com>
2014-05-24 15:34:17 +0400

    Setup labels

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,6 +1,16 @@
 class ListController < UIViewController
   DARK_COLOR_STRING = "#3c3c3c"
 
+  OPTIONS = [
+    "INCH ‹› CENTIMETER",
+    "MILE ‹› KILOMETRE",
+    "FOOT ‹› METER",
+    "CELSIUS ‹› FAHRENHEIT",
+    "POUND ‹› KILOGRAM",
+    "OUNCE ‹› GRAM",
+    "ACRE ‹› HECTARE"
+  ]
+
   def viewDidLoad
     self.view.backgroundColor = UIColor.redColor
     @baseColor = "#48CA77"
@@ -15,7 +25,7 @@ class ListController < UIViewController
   end
 
   def tableView(tableView, numberOfRowsInSection: section)
-    7
+    OPTIONS.size
   end
 
   def tableView(tableView, heightForRowAtIndexPath: indexPath)
@@ -38,6 +48,7 @@ class ListController < UIViewController
     #cell.textLabel.textColor = UIColor.whiteColor
 
     cell.frame = [cell.frame.origin, [@table.frame.size.width, self.view.frame.size.height/7]]
+
     if indexPath.row == 0
       cell.backgroundColor = DARK_COLOR_STRING.to_color
     else
@@ -47,6 +58,23 @@ class ListController < UIViewController
       cell.addSubview(mask)
       cell.backgroundColor = @baseColor.to_color
     end
+
+    labelFrame = [[30, 0], [cell.frame.size.width - 60, cell.frame.size.height]]
+    label = UILabel.alloc.initWithFrame(labelFrame)
+    label.font = UIFont.fontWithName("HelveticaNeue-Medium", size: 20)
+    attributedString = NSMutableAttributedString.alloc.initWithString(OPTIONS[indexPath.row])
+    attributedString.addAttribute(NSKernAttributeName, value: 1.4, range: NSMakeRange(0,9))
+    label.attributedText = attributedString
+    #label.textAlignment = NSTextAlignmentCenter
+
+    if indexPath.row == 0
+      label.textColor = @baseColor.to_color
+    else
+      label.textColor = UIColor.whiteColor
+    end
+
+    cell.addSubview(label)
+
 
     cell
   end

d0a67a6bd244914f829152c0932cd2ee021459c5
Artyom Keydunov <artyom.keydunov@gmail.com>
2014-05-24 14:11:21 +0400

    Setup colors for list

--- app/controllers/list_controller.rb
+++ app/controllers/list_controller.rb
@@ -1,17 +1,25 @@
 class ListController < UIViewController
+  DARK_COLOR_STRING = "#3c3c3c"
+
   def viewDidLoad
     self.view.backgroundColor = UIColor.redColor
+    @baseColor = "#48CA77"
 
     @table = UITableView.alloc.initWithFrame(self.view.bounds, style: UITableViewStylePlain)
 
     @table.delegate = self
     @table.dataSource = self
+    @table.separatorStyle = UITableViewCellSeparatorStyleNone
 
     self.view.addSubview(@table)
   end
 
   def tableView(tableView, numberOfRowsInSection: section)
-    10
+    7
+  end
+
+  def tableView(tableView, heightForRowAtIndexPath: indexPath)
+    self.view.frame.size.height/7
   end
 
   def tableView(tableView, didSelectRowAtIndexPath: indexPath)
@@ -20,9 +28,24 @@ class ListController < UIViewController
 
   def tableView(tableView, cellForRowAtIndexPath: indexPath)
     @reuse_identifier ||= "CELL_IDENTIFIER"
+    # 3c3c3c
 
     cell = tableView.dequeueReusableCellWithIdentifier(@reuse_identifier) || begin
       UITableViewCell.alloc.initWithStyle(UITableViewCellStyleDefault, reuseIdentifier:@reuse_identifier)
+    end
+
+    #cell.textLabel.text = "Title"
+    #cell.textLabel.textColor = UIColor.whiteColor
+
+    cell.frame = [cell.frame.origin, [@table.frame.size.width, self.view.frame.size.height/7]]
+    if indexPath.row == 0
+      cell.backgroundColor = DARK_COLOR_STRING.to_color
+    else
+      mask = UIView.alloc.initWithFrame(cell.bounds)
+      mask.backgroundColor = DARK_COLOR_STRING.to_color
+      mask.alpha = 0.1 * (indexPath.row - 1)
+      cell.addSubview(mask)
+      cell.backgroundColor = @baseColor.to_color
     end
 
     cell
